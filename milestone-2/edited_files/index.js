e109f20d8152ce7f1e3f6d5153171f5c846171a6
Taaruni Ananya <106027676+tanya-ananya@users.noreply.github.com>
2024-11-04 18:43:42 -0600

    Add files via upload

---
+++ milestone-2/edited_files/index.js
@@ -0,0 +1,14 @@
+// TODO: Query for button with an id "theme-button"
+
+
+// TODO: Complete the toggleDarkMode function
+const toggleDarkMode = () => {
+
+    // Write your code to manipulate the DOM here
+
+
+}
+
+
+// TODO: Register a 'click' event listener for the theme button
+// Set toggleDarkMode as the callback function.
